04554b7814a37a9cdfe1d77ab8a4d19243612d7b
PoChuanSu <peterpeter1010@live.com>
2025-02-06 16:42:02 +1100

    product FE/BE setup-follow up: data import to DB

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,15 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/index.css";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index={true} path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
         </Route>
     )
 );
