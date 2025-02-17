711515cc869db46fb6e054de53f6849c922f234e
PoChuanSu <peterpeter1010@live.com>
2025-02-17 16:17:29 +1100

    sign in -fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Outlet />
             </Container>
             <Footer />
+            <ToastContainer />
         </>
     );
 }
